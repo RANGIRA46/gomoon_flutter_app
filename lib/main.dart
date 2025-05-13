9b46ff210f4ea95a80303cf6f0ec892090be2b60
Nobody <johnsonbaraka46@gmail.com>
2025-05-13 21:06:07 +0200

    added safe area

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GoMoon',
-      theme: ThemeData(scaffoldBackgroundColor:  Color.fromRGBO(31, 31, 31, 1.0)),
-      home: Scaffold(),
+      theme: ThemeData(scaffoldBackgroundColor: Color.fromRGBO(31, 31, 31, 1.0)),
+      home: const HomePage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,29 +6,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pageTitle()
+      body: Stack(
+        children: [
+          _astroImageWidget(), // Background image
+          SafeArea(
+            child: Container(
+              child: _pageTitle(), // Centered title
+            ),
+          ),
+        ],
+      ),
     );
   }
-Widget _pageTitle() {
-  return Text(
-    "#GoMoon",
-    style: TextStyle(
-      fontSize: 70,
-      fontWeight: FontWeight.w800,
-      color: Colors.white,
-    ),
-  );
-}
 
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+        fontSize: 70,
+        fontWeight: FontWeight.w800,
+        color: Colors.white,
+      ),
+    );
+  }
 
   Widget _astroImageWidget() {
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
           image: AssetImage('assets/images/home_background.png'),
-          fit:
-              BoxFit
-                  .cover, // Change to `BoxFit.cover` for full-screen background
+          fit: BoxFit.cover,
         ),
       ),
     );
