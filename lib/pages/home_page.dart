1223126e13bc43acc59648a37907397f10d0e2b7
Nobody <johnsonbaraka46@gmail.com>
2025-05-14 10:43:16 +0200

    Exploring drop down buttons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-
-  late double _deviceHeight, _deviceWidth;
+  late final double _deviceHeight, _deviceWidth;
 
   HomePage({super.key});
 
@@ -18,7 +17,30 @@ class HomePage extends StatelessWidget {
             child: Container(
               height: _deviceHeight,
               width: _deviceWidth,
-              child: _pageTitle(), // Centered title
+              padding: EdgeInsets.symmetric(
+                vertical: _deviceHeight * 0.1,
+                horizontal: _deviceWidth * 0.05,
+              ),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(31, 31, 31, 0.8),
+                    Color.fromRGBO(31, 31, 31, 0.5),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _pageTitle(), // Centered title
+                  const SizedBox(
+                    height: 20,
+                  ), // Space between title and dropdown
+                  _destinationDropDownWidget(), // Dropdown widget
+                ],
+              ),
             ),
           ),
         ],
@@ -45,6 +67,33 @@ class HomePage extends StatelessWidget {
           fit: BoxFit.cover,
         ),
       ),
+    );
+  }
+
+  Widget _destinationDropDownWidget() {
+    return DropdownButton<String>(
+      value: null, // Initial value of the dropdown
+      hint: const Text(
+        'Select Destination',
+        style: TextStyle(color: Colors.white),
+      ),
+      dropdownColor: Colors.black,
+      onChanged: (String? newValue) {
+        // Handle selection change
+      },
+      items: const [
+        DropdownMenuItem(
+          value: 'Johnson Webb Station',
+          child: Text(
+            'Johnson Webb Station',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        DropdownMenuItem(
+          value: 'Baraka Station',
+          child: Text('Baraka Station', style: TextStyle(color: Colors.white)),
+        ),
+      ], // Correctly set items as a list of DropdownMenuItem
     );
   }
 }
